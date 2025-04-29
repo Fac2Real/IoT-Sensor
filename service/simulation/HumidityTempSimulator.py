10c2c825fd1324010f1f8590d904619fd0e9fd24
minnnseokk <msjeong991029@gmail.com>
2025-04-29 17:51:52 +0900

    feat    | sprint1 | FRB-121 | 센서 스레드 형식, json 파일로 분리 | 정민석

--- service/simulation/HumidityTempSimulator.py
+++ service/simulation/HumidityTempSimulator.py
@@ -1,9 +1,9 @@
-from .SimulatorInterface2 import SimulatorInterface2
-from simulate_type.simulate_list import generate_humidity_temp_data
+# from .SimulatorInterface2 import SimulatorInterface2
+# from simulate_type.simulate_list import generate_humidity_temp_data
 
-class HumidityTempSimulator(SimulatorInterface2):
-    def generate_data(self, idx: int) -> dict:
-        """
-        Generate a single data entry containing both humidity and temperature.
-        """
-        return generate_humidity_temp_data(idx)
+# class HumidityTempSimulator(SimulatorInterface2):
+#     def generate_data(self, idx: int) -> dict:
+#         """
+#         Generate a single data entry containing both humidity and temperature.
+#         """
+#         return generate_humidity_temp_data(idx)
