270e68fdf96c82917855c75562ed5ee6f877298f
minnnseokk <msjeong991029@gmail.com>
2025-04-29 09:15:24 +0900

    fix | sprint1 | FRB-107 | MQTT 수신 실패 예외 처리 | 정민석

--- service/simulation/simulateTest.py
+++ service/simulation/simulateTest.py
@@ -9,35 +9,20 @@ from mqtt_util.publish import AwsMQTT
 # from simulate_type.simulate_list import generate_temp_data, generate_humidity_data, generate_humidity_temp_data, generate_wearable_data, generate_vibration_data, generate_current_data 
 from .factory import get_simulator
 
-# # 데이터 생성 함수 선택
-# 인터페이스로 추상화함
-# def select_data_generator(simulator_type):
-#     if simulator_type == "temp":
-#         return lambda idx: generate_temp_data(idx)
-#     elif simulator_type == "humidity":
-#         return lambda idx: generate_humidity_data(idx)
-#     elif simulator_type == "humidity_temp":
-#         return lambda idx: generate_humidity_temp_data(idx)
-#     elif simulator_type == "wearable": 
-#         return lambda idx: generate_wearable_data(idx)
-#     elif simulator_type == "vibration":
-#         return lambda idx: generate_vibration_data(idx)
-#     elif simulator_type == "current":
-#         return lambda idx: generate_current_data(idx)
-#     else:
-#         raise ValueError(f"Unknown simulator type: {simulator_type}")
-
 # 시뮬레이션 함수
 def simulate_data(count, interval, sensor_num=2,callback=None, simulator_type="humidity_temp"):
     try:
         print(f"Simulating {simulator_type} data stream for {count} entries with {interval} second intervals... (Press Ctrl+C to stop)")
         # 데이터 생성 함수 선택
-        simulator = get_simulator(simulator_type) # 함수 , 토픽을 반환
-        topic_name = simulator.generate_data(0)["id"] # 첫 번째 센서의 ID를 사용하여 토픽 이름 생성
+        # 시뮬레이터를 타입에 맞게 가져올  인터페이스의 함수 호출
+        simulator = get_simulator(simulator_type)
+        # 첫 번째 센서의 ID를 사용하여 토픽 이름 생성
+        topic_name = simulator.generate_data(0)["id"]
         for _ in range(count):
             for sensor_idx in range(sensor_num):
                 # 선택된 데이터 생성 함수 호출
-                payload = simulator(sensor_idx) # generate_temp_data(sensor_idx)와 같은 형태로 호출됨
+                # generate_data(sensor_idx)와 같은 형태로 호출됨
+                payload = simulator.generate_data(sensor_idx)
                 # 콜백이 주어지면 해당 콜백을 호출하여 데이터를 전달
                 if callback and topic_name:
                     callback(payload, topic_name)
